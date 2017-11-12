2cf396f9636e14ef0abce240734a3d90a600de6d
srajput <sujitsing.rajput@coxautoinc.com>
2017-11-11 23:05:10 -0500

    autocommit

---
+++ autocommit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Automatically merge the last commit through the following branches:
+# 2.1 -} 2.2 -} 2.3 -} master
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LAST_COMMIT=$(git rev-list -1 HEAD)
+
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to master
+
+ git checkout master && git merge $CURRENT_BRANCH
+  git checkout $CURRENT_BRANCH
